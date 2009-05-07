7c3426b00e74a43306ca19f81e3fcf78dc53e76c
Lim Chee Aun <cheeaun@gmail.com>
2009-05-08 01:40:40 +0800

    Adapted to the new file/folder structure.
    Signed-off-by: Elliott Kember <elliott.kember@gmail.com>

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,32 @@
-/* Your code goes here! */
+var kemberIdentity = function(kemberEl){
+
+	var randomGenerate = function(len){
+		var chars = '0123456789abcdef';
+		var str = '';
+		for (var i=0, clen=chars.length; i<len; i++) {
+			var r = Math.floor(Math.random() * clen);
+			str += chars.substring(r, r+1);
+		}
+		return str;
+	};
+	
+	var i = 0;
+	// using setInterval because `while` loop hangs the browser. Bad...
+//	while (true){
+	var interval = setInterval(function(){
+		var str = randomGenerate(32);
+		i++;
+		if (i == 10000){
+			kemberDiv.innerHTML += str + '<br>';
+			i = 0;
+		}
+		var output = hex_md5(str);
+		if (output == str){
+			kemberDiv.innerHTML += '<strong>' + str + ' is a reversible md5 hash!</strong>';
+			clearInterval(interval);
+//		break;
+		}
+	}, 1);
+//	}
+
+};
