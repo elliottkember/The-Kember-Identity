1fecc0d8b8eecc97b268c89abaec0953a1420ef2
Markus Anders <andoro@freenet.de>
2009-05-11 00:32:33 +0800

    edited ruby.rb so it calculates speed
    Signed-off-by: Elliott Kember <elliott.kember@gmail.com>

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -4,16 +4,20 @@
 # by Charlie Smurthwaite
 
 require 'digest/md5'
+require 'find_speed.rb'
 i = 0
 j = 0
+hashes_before_output = 10000
+speed = FindSpeed.new( hashes_before_output )
 while i <= 340282366920938463463374607431768211455
   i = i + 1
   j = j + 1
   plain = i.to_s(16).rjust(32,'0')
   digest = Digest::MD5.hexdigest(plain)
   break if plain == digest
-  if j==10000
-    puts plain + " " + digest
+  if j==hashes_before_output
+    puts plain + " " + digest + " @ #{speed.stop} hashes/s"
+    speed.start
     j=0
   end
 end
