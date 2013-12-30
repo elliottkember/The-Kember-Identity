2a9e0d49cde252364be292bc26e1bca983a90c7d
Ing. Jan Kaláb <pitlicek@gmail.com>
2013-12-30 15:22:12 +0100

    Naive implementation in Dart

---
+++ dart/dart-Jan-Kalab.dart
@@ -0,0 +1,14 @@
+import 'package:crypto/crypto.dart' show MD5, CryptoUtils;
+import 'dart:math' show Random;
+
+void main() {
+	Random r = new Random();
+	while (true) {
+		List<int> inhash = new List<int>.generate(16, (int index) => r.nextInt((1<<32)-1));
+		MD5 md5 = new MD5();
+		md5.add(inhash);
+		List<int> outhash = md5.close();
+		print(CryptoUtils.bytesToHex(outhash));
+		if (inhash == outhash) break;
+	}
+}
